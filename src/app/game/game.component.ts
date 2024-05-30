eb15982f33a9048401cdabd4a58af2749bdcb12e
Hasan <hasan.celik1501@gmail.com>
2024-05-30 22:00:34 +0200

    Update card component with dynamic content
    The card component's static content was replaced with dynamic 'ng-content' allowing for a more flexible component usage. Moreover, this change removes the hardcoded '500 Punkte' content from the card component and introduces it back in the game component using the newly flexible card component.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgTemplateOutlet} from "@angular/common";
 import {CardComponent} from "../card/card.component";
 
 @Component({
-  selector: 'app-game',
-  standalone: true,
-  imports: [
-    NgTemplateOutlet,
-    CardComponent
-  ],
-  templateUrl: './game.component.html',
-  styles: ``
+    selector: 'app-game',
+    standalone: true,
+    imports: [
+        NgTemplateOutlet,
+        CardComponent
+    ],
+    templateUrl: './game.component.html',
+    styles: ``
 })
 export class GameComponent {
 
