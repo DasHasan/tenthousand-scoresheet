5fefa71d8dad9c85a372548528a726908713344f
Hasan <hasan.celik1501@gmail.com>
2024-08-28 23:39:21 +0200

    Added score input validation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,21 +1,48 @@
 import { Component, effect, inject } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import {
+    AbstractControl,
+    AsyncValidatorFn,
+    FormBuilder,
+    FormsModule,
+    ReactiveFormsModule,
+    ValidationErrors,
+    Validators
+} from "@angular/forms";
 import { CardComponent } from "../card/card.component";
 import { GameService } from "../game.service";
 
 @Component({
     selector: "app-game",
     standalone: true,
-    imports: [FormsModule, CardComponent],
+    imports: [FormsModule, CardComponent, ReactiveFormsModule],
     templateUrl: "./game.component.html",
     styles: ``
 })
 export class GameComponent {
-    currentScore?: number;
+    private readonly fb = inject(FormBuilder);
+    private readonly gameService = inject(GameService);
 
-    gameService = inject(GameService);
+    readonly game = this.gameService.game;
 
-    game = this.gameService.game;
+
+    buildScoreValidator = (): AsyncValidatorFn => async (control: AbstractControl<number>): Promise<ValidationErrors | null> => {
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        const hasMinBound = value > 350;
+        const isDivisible = value / 5 % 1 === 0;
+
+        const scoreValid = hasMinBound && isDivisible;
+
+        return !scoreValid ? { scoreValid: true } : null;
+    };
+
+    scoreForm = this.fb.group({
+        score: this.fb.nonNullable.control<number | undefined>(undefined, Validators.required, this.buildScoreValidator())
+    });
 
     constructor() {
         effect(() => {
@@ -29,17 +56,23 @@ export class GameComponent {
     }
 
     addScore() {
+        if (!this.scoreForm.valid) {
+            return;
+        }
+
+        const score = this.scoreForm.get("score")?.value!;
+
         this.game.update(game => !game ? game : ({
             ...game,
             currentPlayerIndex: (game.currentPlayerIndex + 1) % game.players.length,
             players: game.players.map((player, i) => i !== game.currentPlayerIndex ? player : ({
                 ...player,
-                scores: [...player.scores, this.currentScore!],
-                sum: player.sum + this.currentScore!
+                scores: [...player.scores, score],
+                sum: player.sum + score
             }))
         }));
 
-        this.currentScore = undefined;
+        this.scoreForm.reset();
     }
 
 }
